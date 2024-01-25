56fd1565e95daced3ed774c52a100282b226c9b2
Gemstone-t <crystaldev1003@gmail.com>
2024-01-25 09:18:08 -0500

    hero responsible complete

--- js/landing.js
+++ js/landing.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   var hero_slider = new Splide('#hero_slider', {
     type: 'loop',
+    focus: 'left',
+
     perPage: 3,
     gap: '1rem',
-    drag: 'free',
     perMove: 1,
-
-    autoplay: true,
+    height: '100%',
     breakpoints: {
       1200: { perPage: 2, gap: '1rem' },
       768: {
         perPage: 1,
-        padding: '5rem'
+        padding: '2rem'
       }
     }
   })
